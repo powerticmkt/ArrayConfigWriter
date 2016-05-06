0c049163d50974367de4cdb63c112883291bc22e
Wakeel Ogunsanya <wakeel.oguns@gmail.com>
2016-05-06 16:39:40 +0100

    Update class-array-config-writer.php
        ===   bug fix ===
    1. Fixed bug where new variable is added to config if the variable to update is not specified 
    
      ===    New Features ====
    
    1. The writer will add comment only if comment parameter of write() method is set and its array 
    
    2. You can decide not to allow the library auto update the file by setting the 
        third parameter of the construct to false

--- class-array-config-writer.php
+++ class-array-config-writer.php
@@ -4,33 +4,52 @@
 Copyright 2016 Wakeel Ogunsanya
 Licensed under GPLv2 or above
 
-Version 1.0.0
+Version 1.1.0
 */
 
 class Array_Config_Writer {
     
     
     /**
-     *
+     * The config file to write
+     * 
+     * set during with class construct
      * @var strng 
      */
     protected $_file ;
     
     /**
-     *
+     * Content read from file
      * @var string 
      */
     protected $_fileContent;
 
     
     /**
-     *
+     * The variable to search for in the file
+     * 
+     * eg if we can to update 'option_name' value in a file with
+     * $config['option_name'] array, the variable is 'config'
+     * 
+     * @note we assume each option index is on seperate line like 
+     *  $config['option_name_one']  = 'one' ;
+     *  $config['option_name_two']  = 'two' ;
+     * 
+     * if you have array like $config['options'] = array(
+     *                                      'option_name_one' => 'one',
+     *                                      'option_name_two' => 'two'
+     *                                 );
+     * You have to first get $config['options'] in variable then manually update 
+     * the elements, after which you will can use this config writer to update the
+     * $config['options'] element
+     * 
      * @var string 
      */
     protected $_variable ;
 
     /**
-     *
+     * The target index of the variable
+     * 
      * @var string 
      */
     protected $_index ;
@@ -47,6 +66,17 @@ class Array_Config_Writer {
      */
     protected $_lastError;
     
+    
+    /**
+     * This option determines whether the file should be updated automatically
+     * 
+     * if set to false, you will manually call save() method after you 
+     * have done your writing with write() method
+     * 
+     * @since 1.1.0
+     * @var boolean 
+     */
+    protected $_autoSave = true ;
 
 
     /**
@@ -54,17 +84,25 @@ class Array_Config_Writer {
      * @param string $config_file Asolute path to config file
      * @param string $variable_name the name of the config varible to update
      */
-    public function __construct($config_file , $variable_name = '\$config' ) 
+    public function __construct($config_file , $variable_name = '\$config' , $auto_save = true ) 
     {
         $this->_file = $config_file ;
+        $this->_autoSave = $auto_save ;
         $this->setVariableName($variable_name );
         
         if ( ! file_exists($this->_file))
         {
             //throw new Exception('Config Write Error: Config file doesnt exists ' . $this->_file);
             $this->_lastError = 'Config Write Error: Config file doesnt exists ' . $this->_file ;
+        
             return ;
         }
+        if ( ! $variable_name)
+        {
+            $this->_lastError = 'You must set the set parameter of the library construct has varible to update' ;
+            return ;
+        }
+        
         $this->_fileContent =  file_get_contents( $this->_file ) ;
         
     }
@@ -80,9 +118,11 @@ class Array_Config_Writer {
      *
      * @param mixed $replacement
      * @param boolean $skip_if_exists Skip updating item if already exists
-     * @param null|array $comments Comment to add to the top of item, each element
+     * @param null|array $comments Comment to add to the top of item (new item), each element
      *  will be placed oon a new line. *  is added before each line , meaning
-     *  you dont have to put /** or *  unleass you want it as a comment
+     *  you dont have to put /** or *  unless you want it show 
+     * 
+     * @note you can not update existing item comment
      *  
      * 
      * @return boolean
@@ -90,7 +130,17 @@ class Array_Config_Writer {
      */
     public function write( $index  = null, $replacement = null , $skip_if_exists = FALSE , $comments = null)
     {
+        // error exists in the constructor?
+        if($this->_lastError)
+        {
+            return $this;
+        }
         
+        if(!$index)
+        {
+            $this->_lastError = 'You must set index you want to update' ;
+            return $this;
+        }
         $prefix = $this->_variable  ;
         
         $regex = '#(' . $prefix  . ')(';
@@ -123,18 +173,19 @@ class Array_Config_Writer {
             // new item here 
             $mark .= var_export( $replacement , true ) . ' ;' ;
             $mark .= "\n" ;
-           // add the comment 
-            $comment_str = '/**' ;
-            $comment_str .= "\n" ;
-            if( $comments )
+           
+            if(is_array($comments) && count($comments) > 0 )
             {
+                // add the comment 
+                $comment_str = '/**' ;
+                $comment_str .= "\n" ;
                 foreach ($comments as $line)
                 {
                     $comment_str .= ' * ' . $line . "\n" ;
                 }
+                // close the comment
+                $comment_str .= '*/'  . "\n";
             }
-            // close the comment
-            $comment_str .= '*/'  . "\n";
             
              // lets try remove traling slash from the variable name since 
             // we are writing php here 
@@ -142,6 +193,9 @@ class Array_Config_Writer {
             {
                 $mark = substr($mark, 1 );
             }
+            // we have updated the index, the file will save automatically in the class shutdwon
+            // we did this allow update multiple index by call write() method as manay times 
+            // as required
             $this->_fileContent = $this->_fileContent . "\n" . $comment_str . "\n"  . $mark . "\n" ;
         }
         return $this;
@@ -178,19 +232,40 @@ class Array_Config_Writer {
         }
         
         $this->_variable = $name;
-        return $this;
     }
 
     /**
      * We can now update the file content
      * 
-     * This allow calling Array_Config_Writer::write() as many times as required
+     * if the _autosave property is true, we auto updte file
+     * 
      */
-    public function __destruct() {
-        file_put_contents( $this->_file , $this->_fileContent ) ;
+    public function __destruct() 
+    {
+        if($this->_autoSave)
+        {
+            $this->save();
+        }
+    }
+    
+    /**
+     * Save the new content to file 
+     * 
+     * You can call Array_Config_Writer::write() as many times as required
+     * before calling this method
+     * 
+     * @return \Array_Config_Writer
+     */
+    public function save()
+    {
+        if(!$this->_lastError)
+        {
+            file_put_contents( $this->_file , $this->_fileContent ) ;
+        }
+        return $this;
     }
 
-   
+    
     /**
      * Get last error that occured
      * 
